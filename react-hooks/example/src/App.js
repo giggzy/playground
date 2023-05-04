e6dadfbf2585c9e8812bcc1f95264497ac2f4899
Gabriel Farrell <gabriel.farrell@gmail.com>
2023-05-04 11:28:55 -0400

    useState for a simple counter.

--- react-hooks/example/src/App.js
+++ react-hooks/example/src/App.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
+
+  const [count, setCount] = useState(4);
+  const [theme, setTheme] = useState('blue');
+
+  function decrementCount() {
+    setCount(prevCount => prevCount - 1);
+  }
+
+  function incrementCount() {
+    setCount(prevCount => prevCount + 1);
+  }
+
   return (
     <>
-      <button>+</button>
-      <span>0</span>
-      <button>-</button>
+      <button onClick={decrementCount}>-</button>
+      <span>{count}</span>
+      <button onClick={incrementCount}>+</button>
+      <br />
+      <span>{theme}</span>
     </>
   );
 }
