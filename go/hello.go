1fed47292c26357df4a915e1df12e925efa435d0
Farrell, Gabriel <Gabriel.Farrell@nyulangone.org>
2023-07-23 09:39:49 -0400

    Add go and python playgrounds.

---
+++ go/hello.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func get_text_to_print() string {
+	var name string = "Gabriel"
+	return "Hello, World!, my name is " + name
+}
+
+func main() {
+	fmt.Println(get_text_to_print())
+}
+*/
+// write a test for get_text_to_print
+// Path: hello_test.go
+
