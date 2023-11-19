fed64fdbed9fad568b1f937cafc8498afb387fea
Farrell, Gabriel <Gabriel.Farrell@nyulangone.org>
2023-11-18 20:24:05 -0500

    some files that were not checked in

--- python_explore/shell.nix
+++ python_explore/shell.nix
@@ -4,9 +4,14 @@ stdenv.mkDerivation {
   name = "python-env";
   nativeBuildInputs = [
     python3
+    python3Packages.pip
     python3Packages.numpy
     python3Packages.pandas
     python3Packages.scipy
     python3Packages.matplotlib
+  ];
+  # for python packages with C++ extensions need to add the following hack
+  NIX_CFLAGS_COMPILE = pkgs.lib.optionals pkgs.stdenv.isDarwin [
+    "-I${pkgs.lib.getDev pkgs.libcxx}/include/c++/v1"
   ];
 }
